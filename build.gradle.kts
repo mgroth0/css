bc2149fe84dc723f2499d950634eb7e376a20a6d
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-18 11:53:09 -0400

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+  sourceSets {
+	val commonMain by getting {
+	  dependencies {
+		implementations(
+		  libs.kotlinx.html.common,
+		  libs.kotlinx.serialization.json,
+		  ":k:klib".auto(), // this is just an example, feel free to remove
+		  handler = this
+		)
+	  }
+	}
+  }
+}
